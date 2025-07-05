3e2ba818e012351116d027aef4ceadabfc8011c2
Keagon King <keagonk@gmail.com>
2025-07-05 08:21:45 -0500

    restructured project

--- build/KrakenEngine/main.h
+++
@@ -1,7 +0,0 @@
-﻿// KrakenEngine.h : Include file for standard system include files,
-// or project specific include files.
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿// main.cpp : Defines the entry point for the application.
+#include "main.h"
+
+
+using namespace std;
+
+int main()
+{
+	Render render;
+	render.initialize();
+
+	// Set the window title
+	render.window.setTitle("Kraken Engine - Debug");
+
+	while(true) {
+		// Main loop behavior
+		MainLoopBehaviour();
+		render.renderFrame();
+	}
+
+	// Shutdown the rendering system
+	render.shutdown();
+
+	// Exit the application
+	cout << "Exiting Kraken Engine..." << endl;
+	return 0;
+}
+
+
+
+void MainLoopBehaviour() {
+	/* Here the main loop will take place. This is not going to be an issue for a while
+	* as I just want to focus on laying the foundation with mathematics and rendering first.
+	*/
+}
